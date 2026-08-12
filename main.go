9fa850ba5cd0ed6761b7eb504fbace75a6f614b8
Dirk Arnez <smalldirkalex@gmail.com>
2026-08-12 16:47:08 +0800

    - update main.go

--- main.go
+++ main.go
@@ -8,6 +8,25 @@ import (
 	"github.com/diskfs/go-diskfs"
 )
 
+/*
+    // 1. 在 Windows 上直接建立一個 500MB 的空白大檔案（取代 fallocate/dd）
+    var diskSize int64 = 500 * 1024 * 1024
+    d, _ := diskfs.Create("sdcard.img", diskSize, diskfs.SectorSize512)
+
+    // 2. 直接在檔案內部寫入 MBR 分割表
+    table := &mbr.Table{
+        Partitions: []mbr.Partition{
+            {Bootable: true, Type: mbr.Fat16, StartReader: 2048, Size: 65536}, // Boot 分區
+            {Bootable: false, Type: mbr.Linux, StartReader: 67584, Size: 400000}, // Rootfs 分區
+        },
+    }
+    d.Partition(table)
+
+    // 3. 在指定分區內直接寫入 FAT 檔案系統，並把 Windows 本地的 zImage 塞進去
+    fs, _ := d.CreateFilesystem(disk.FilesystemSpec{Partition: 1, FSType: diskfs.TypeFat32})
+    f, _ := fs.OpenFile("/zImage", os.O_CREATE|os.O_WRONLY)
+    // ...隨後直接利用 Go 的 io.Copy 將核心寫入，完全不需要 mount
+*/
 func main() {
 	// 1. 打開你已經創建好的映像檔（或是新創建的檔案）
 	diskImg, err := diskfs.Open("sdcard.img")
